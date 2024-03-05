f7c4a4cafdbfd6c1d4437cd981008359bae50231
washington maina <103877612+washington254@users.noreply.github.com>
2024-03-05 15:16:52 +0000

    feat: Update webpack-dev-server command to include port 8080, update typescript version from 3.9.9 to 5.0.3 and refactor webpack.dev.js to use double quotes and consistent formatting

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,12 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        // progress: true,
-        liveReload: false
-    },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    port: 8080,
+    liveReload: false,
+    disableHostCheck: true, // Add this line to allow requests from any host
+  },
 });
